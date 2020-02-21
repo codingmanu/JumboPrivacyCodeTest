3153055047a55abdeebe50e0fb08ac84b1dca116
Manuel Gomez <atcodingmanu@gmail.com>
2020-02-21 11:32:00 -0500

    Initial Commit

---
+++ JumboPrivacy-CodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JumboPrivacy-CodeTest
+//
+//  Created by Manuel S. Gomez on 2/21/20.
+//  Copyright © 2020 codingManu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
